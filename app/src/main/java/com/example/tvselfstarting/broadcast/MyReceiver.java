5cfb7499f1d668d3e07f8ff165c3ae745a9947aa
小石 <2665867688@qq.com>
2018-01-16 09:10:43 +0800

    Update MyReceiver.java

--- app/src/main/java/com/example/tvselfstarting/broadcast/MyReceiver.java
+++ app/src/main/java/com/example/tvselfstarting/broadcast/MyReceiver.java
@@ -18,7 +18,7 @@ import com.example.tvselfstarting.utils.DataUtils;
  * description:捕捉Android 设备开机启动时的广播 然后启动要启动的app
  */
 public class MyReceiver extends BroadcastReceiver {
-    //Android设备开机是会发送的一个广播
+    //Android设备开机是会发送的一个广播 需要加一个挂载点被挂载到手机上的一个广播才行
     private static final String ACTION = "android.intent.action.BOOT_COMPLETED";
     private String appPackageName;
     private String firstActivityName;
